966a1d9056f3fd4a93203fc6b0bf6d8bb4b842c9
NextFire <git@yuru.moe>
2023-09-18 17:21:39 -0400

    basic flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+
+from flask import Flask
+
+INSTANCE_NUMBER = os.environ.get("INSTANCE_NUMBER", "unknown")
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return f"Instance number {INSTANCE_NUMBER} is responding now!"
